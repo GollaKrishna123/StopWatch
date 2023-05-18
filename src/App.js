e054089153e22ae59987ce9a9ad2cc14e83ee601
GollaKrishna123 <gkrishna05348@gmail.com>
2023-05-18 15:12:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 const App = () => {
   const [seconds, setSeconds] = useState(0);
   const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
   const [running, setRunning] = useState(false);
 
   useEffect(() => {
@@ -27,6 +28,7 @@ const App = () => {
     setRunning(false);
     setSeconds(0);
     setMinutes(0);
+    setHours(0);
   };
 
   if (seconds > 59) {
@@ -34,17 +36,28 @@ const App = () => {
     setMinutes(minutes + 1);
   }
 
+  if (minutes > 59) {
+    setMinutes(0);
+    setHours(hours + 1);
+  }
+
   return (
     <div className="timer-container">
       <div>
-        <h1 style={{ textAlign: "center",fontSize : "50px", paddingBottom: "20px" }}>
+        <h1
+          style={{
+            textAlign: "center",
+            fontSize: "50px",
+            paddingBottom: "20px",
+          }}
+        >
           StopWatch
         </h1>
         <div className="timer-shadow">
           <h2 className="timer">Timer</h2>
-          <h1 className="time">{`${minutes < 10 ? `0${minutes}` : minutes}:${
-            seconds < 10 ? `0${seconds}` : seconds
-          }`}</h1>
+          <h1 className="time">{`${hours < 10 ? `0${hours}` : hours}:${
+            minutes < 10 ? `0${minutes}` : minutes
+          }:${seconds < 10 ? `0${seconds}` : seconds}`}</h1>
           <button onClick={startHandler} className="start-btn">
             Start
           </button>
